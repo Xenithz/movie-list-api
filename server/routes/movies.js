0553bb2ce0cb85b177389c51c72c4a5ffab41395
Angelo Guerrero <amcgdev@gmail.com>
2019-08-13 22:35:02 +0400

    added get routes to support query params for genre, title, and director

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -3,14 +3,48 @@ const movieRouter = new Router();
 const config = require('./api_config');
 const dbWrapper = require('./queries');
 
-movieRouter.get(`${config.apiURL}/movies`, async (ctx) =>{
+movieRouter.get(`${config.apiURL}/movies`, async (ctx, next) => {
+    if(ctx.query.genre != null) {
+        const movies = await dbWrapper.getMovieByGenre(ctx.query.genre);
+        ctx.body = movies;
+    }
+    else {
+        await next();
+    }
+});
+
+movieRouter.get(`${config.apiURL}/movies`, async (ctx, next) => {
+    if(ctx.query.title != null) {
+        const movies = await dbWrapper.getMovieByTitle(ctx.query.title);
+        ctx.body = movies;
+    }
+    else {
+        await next();
+    }
+});
+
+movieRouter.get(`${config.apiURL}/movies`, async (ctx, next) => {
+    console.log('hey');
+    if(ctx.query.director != null) {
+        console.log('heyyyy');
+        const movies = await dbWrapper.getMovieByDirector(ctx.query.director);
+        ctx.body = movies;
+    }
+    else {
+        await next();
+    }
+});
+
+movieRouter.get(`${config.apiURL}/movies`, async (ctx) => {
+    console.log('hi');
     const movies = await dbWrapper.getAllMovies();
     ctx.body = movies;
 });
 
-movieRouter.get(`${config.apiURL}/movies/:id`, async (ctx) =>{
+movieRouter.get(`${config.apiURL}/movies/:id`, async (ctx) => {
     const movie = await dbWrapper.getMovieByID(ctx.params.id);
     ctx.body = movie;
 });
+
 
 module.exports = movieRouter;
--- server/routes/queries.js
+++ server/routes/queries.js
@@ -1,26 +1,59 @@
 const pool = require('./db_config');
 
 async function getAllMovies() {
-    try{
+    try {
         const movies = await pool.query('SELECT * FROM movies ORDER BY id ASC');
-        return movies;
+        return movies.rows;
     }
-    catch{
+    catch {
         console.log('failed db');   
     }
 }
 
 async function getMovieByID(movieID) {
-    try{
-        const movies = await pool.query(`SELECT * FROM movies WHERE movie_id = ${movieID}`);
-        return movies;
+    try {
+        const movie = await pool.query('SELECT * FROM movies WHERE movie_id = $1', [movieID]);
+        return movie.rows;
     }
     catch{
         console.log('failed db');   
     }
 }
 
+async function getMovieByTitle(movieTitle) {
+    try {
+        const movies = await pool.query('SELECT * FROM movies WHERE movie_title = $1', [movieTitle]);
+        return movies.rows;
+    }
+    catch {
+        console.log('failed db');
+    }
+}
+
+async function getMovieByGenre(movieGenre) {
+    try {
+        const movies = await pool.query('SELECT * FROM movies WHERE movie_genre = $1', [movieGenre]);
+        return movies.rows;
+    }
+    catch {
+        console.log('failed db');
+    }
+}
+
+async function getMovieByDirector(movieDirector) {
+    try {
+        const movies = await pool.query('SELECT * FROM movies WHERE movie_director = $1', [movieDirector]);
+        return movies.rows;
+    }
+    catch {
+        console.log('failed db');
+    }
+}
+
 module.exports = {
     getAllMovies,
-    getMovieByID
+    getMovieByID,
+    getMovieByTitle,
+    getMovieByGenre,
+    getMovieByDirector
 };
