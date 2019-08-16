1ac4447edf5b88f08daebb45863ef80971a51f3a
Angelo Guerrero <amcgdev@gmail.com>
2019-08-16 15:05:32 +0400

    Stored local database credentials in environment variables with .env file (dotenv dependency added), added db_config

---
+++ server/routes/db_config.js
@@ -0,0 +1,24 @@
+const Pool = require('pg').Pool;
+require('dotenv').config();
+const env = process.env.NODE_ENV || 'development';
+
+let connection = {};
+
+if(env === 'development') {
+  connection = {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    }
+}
+else {
+  connection = {
+    connectionString: process.env.DATABASE_URL
+  }
+}
+
+const pool = new Pool(connection);
+
+module.exports = pool;
