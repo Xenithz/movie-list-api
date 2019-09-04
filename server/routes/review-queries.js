3b5d9c31e5cd9002e097b93bc40b62a62e9dc87a
Angelo Guerrero <amcgdev@gmail.com>
2019-09-04 15:42:23 +0400

    Testing travis interaction with postgresql setup

--- server/routes/review-queries.js
+++ server/routes/review-queries.js
@@ -18,12 +18,22 @@ async function clearTable() {
 async function insertIntoTable(refCode, review, score, movieName, reviewerName) {
     await pool.query(`INSERT INTO reviews (ref_code, review, score, movie_name, reviewer_name)
     VALUES ($1, $2, $3, $4, $5)`, [refCode, review, score, movieName, reviewerName]);
+}
 
-    console.log('inserted');
+function createTable() {
+    pool.query(`CREATE TABLE reviews (
+        id SERIAL,
+        ref_code text,
+        review text,
+        score integer,
+        movie_name text,
+        reviewer_name text
+        )`);
 }
 
 module.exports = {
     getAllReviews,
     clearTable,
-    insertIntoTable
+    insertIntoTable,
+    createTable
 };
